1b6b9e60466754e7d0d341d85c56476b256a2246
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-08-20 23:43:21 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // code your solution here
+// const record = [
+//     { year: "2015", result: "W"},
+//     {year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"}
+
+// ]
+
+// function superbowlWin(record) {
+//     // const finding = record.find( function (record) {
+//     //     record.finding === "W"; // 
+//     // });
+
+//     // if(!!finding) {
+//     //     return finding.year
+//     // } else {
+//     //     return undefined
+//     // }
+//     return !!result ? result.year : undefined;
+
+// }
+
+
+//    superbowlWin(record) {
+//     const result = record.find( function (record) {
+//          record.result === "W"
+//     });
+//     return !!result ? result.year : undefined;
+//   }
+
+superbowlWin = (record) => {
+    const result = record.find(record => record.result === "W");
+     
+    if (result) {
+        return result.year
+    } else{
+        return undefined;
+    } 
+  }
